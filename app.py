64ba20d1ed030d35a294c1bb6e943f1402183203
Ridho Pratama <p.ridho@yahoo.co.id>
2020-02-14 20:25:50 +0700

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-from tkinter import N, W, E, S, HORIZONTAL, BOTH, LEFT
+from tkinter import N, W, E, S, HORIZONTAL, BOTH, LEFT, END
 
 from PIL import Image, ImageTk
 
@@ -14,13 +14,20 @@ class App(tk.Frame):
         self.columnconfigure(1, weight=5)
 
         self.src_filename = tk.StringVar()
+        self.src_filename.trace("w", self.on_src_filename_write)
         self.show_result = tk.BooleanVar()
+        self.show_result.trace("w", self.on_show_result_write)
         self.message = tk.StringVar()
+        self.message.trace("w", self.on_message_write)
+        self.password = tk.StringVar()
+        self.password.trace("w", self.on_password_write)
         self.bitlen = tk.IntVar()
+        self.bitlen.trace("w", self.on_bitlen_write)
         self.mode = tk.StringVar(value="encode")
 
         self.original_image = None
         self.processed_image = None
+        self.current_imagetk = None
 
         self.create_widgets()
     
@@ -34,8 +41,9 @@ class App(tk.Frame):
         src_filename_label.grid(row=0, column=1, sticky=E+N)
 
         # Image widget row 1
-        image_label = tk.Label(self, text="Image might be here later", relief="sunken")
-        image_label.grid(row=1, column=0, columnspan=2, sticky=N+E+W+S)
+        self.image_label = tk.Label(
+            self, text="Image might be here later", relief="sunken")
+        self.image_label.grid(row=1, column=0, columnspan=2, sticky=N+E+W+S)
 
         # Show Image Result Checkbox
         show_result_checkbox = tk.Checkbutton(
@@ -43,21 +51,24 @@ class App(tk.Frame):
         show_result_checkbox.grid(row=2, column=0, sticky=W)
 
         # Message to hide
-        message_text = tk.Text(self, height=5)
-        message_text.grid(row=3, column=0, columnspan=2, sticky=W+E)
+        # self.message_entry = tk.Text(self, height=5)
+        # self.message_entry.grid(row=3, column=0, columnspan=2, sticky=W+E)
+        tk.Label(self, text="Message: ").grid(row=3, column=0, sticky=W)
+        self.message_entry = tk.Entry(self, textvariable=self.message)
+        self.message_entry.grid(row=3, column=1, sticky=W+E)
 
         # Password entry
         tk.Label(self, text="Password: ").grid(row=4, column=0, sticky=W)
-        password_entry = tk.Entry(self)
-        password_entry.grid(row=4, column=1, sticky=W)
+        password_entry = tk.Entry(self, textvariable=self.password)
+        password_entry.grid(row=4, column=1, sticky=W+E)
     
         # Bit length to be used
         bitlen_scale = tk.Scale(
             self, from_=1, to=4, orient=HORIZONTAL, label="Bit Used:", variable=self.bitlen)
         bitlen_scale.grid(row=5, column=0, sticky=W)
 
-        stats_label = tk.Label(self, text="Stats about images might be here\nlater")
-        stats_label.grid(row=5, column=1, sticky=W)
+        self.stats_label = tk.Label(self, text="Stats about images might be here later", justify=LEFT)
+        self.stats_label.grid(row=5, column=1, sticky=W)
 
         # Radio button for selecting encode/decode
         tk.Radiobutton(
@@ -72,6 +83,62 @@ class App(tk.Frame):
         save_button.grid(row=7, column=0, columnspan=2, sticky=E)
 
     def select_file(self):
-        print("Select file")
-        self.src_filename.set(askopenfilename())
-        print(f"{self.src_filename.get()=}")
+        filename = askopenfilename(filetypes=[("Image","*.png"), ("Image","*.bmp")])
+        self.src_filename.set(filename)
+    
+    def on_src_filename_write(self, *args):
+        filename = self.src_filename.get()
+        print(f"src_filename change {filename=}")
+
+        self.original_image = Image.open(filename)
+        self.processed_image = self.original_image.copy()
+
+        self.reset_options_state()
+        self.update_stats_label()
+    
+    def update_stats_label(self, *args):
+        img_w, img_h = self.original_image.size
+        avl_bit = img_w * img_h * self.bitlen.get()
+        avl_byte = avl_bit // 8
+
+        message = self.message.get()
+        message_bytes = message.encode("utf-8")
+        msg_len = len(message)
+        msg_byte = len(message_bytes)
+        byte_left = avl_byte - msg_byte
+
+        password = self.password.get()
+        enc = "encrypted" if password != "" else "not encrypted"
+
+        result_text = f"size={img_w}x{img_h} {avl_bit=}bits {avl_byte=}bytes"
+        result_text += f"\n{msg_len=} {msg_byte=} {byte_left=}bytes"
+        result_text += f"\n{password=} {enc}"
+
+        self.stats_label["text"] = result_text
+    
+    def reset_options_state(self):
+        self.show_result.set(False)
+        self.on_show_result_write()
+        self.bitlen.set(1)
+        self.mode.set("encode")
+    
+    def on_show_result_write(self, *args):
+        self.image_label["image"] = None
+        if self.show_result.get():
+            image = self.original_image
+        else:
+            image = self.processed_image
+        self.show_image_on_label(image)
+    
+    def on_bitlen_write(self, *args):
+        self.update_stats_label()
+
+    def show_image_on_label(self, image):
+        self.current_imagetk = ImageTk.PhotoImage(image)
+        self.image_label["image"] = self.current_imagetk
+    
+    def on_message_write(self, *args):
+        self.update_stats_label()
+    
+    def on_password_write(self, *args):
+        self.update_stats_label()
