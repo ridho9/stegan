28b28f13cd8af009f1c80730c3f1956e49fb7a47
Ridho Pratama <p.ridho@yahoo.co.id>
2020-02-17 11:42:19 +0700

    Fix PSNR crash on other image mode

--- stegan/psnr.py
+++ stegan/psnr.py
@@ -10,17 +10,24 @@ def mse(img1, img2):
     data1 = img1.getdata()
     data2 = img2.getdata()
 
+    mode = img1.mode
+
     w, h = size1
     total = 0
     for x in range(w):
         for y in range(h):
             index = x + w*y
-            r1, g1, b1 = data1[index][:3]
-            r2, g2, b2 = data2[index][:3]
-            dr = (r1 - r2)**2
-            dg = (g1 - g2)**2
-            db = (b1 - b2)**2
-            total += dr + dg + db
+            if mode in ["RGB", "RGBA"]:
+                r1, g1, b1 = data1[index][:3]
+                r2, g2, b2 = data2[index][:3]
+                dr = (r1 - r2)**2
+                dg = (g1 - g2)**2
+                db = (b1 - b2)**2
+                total += dr + dg + db
+            elif mode in ["L", "P"]:
+                p1 = data1[index]
+                p2 = data2[index]
+                total += (p1 - p2) ** 2
     
     total /= w * h
     total /= 3
